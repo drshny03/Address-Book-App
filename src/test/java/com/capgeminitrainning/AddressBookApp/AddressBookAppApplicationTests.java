c7ca287dac64c7dc9e877d8bec5d6575377943e1
drshny03 <darshanyadav7049@gmail.com>
2025-02-24 13:06:50 +0530

    [Darshan] Add . Create a Address Book Spring Project to

---
+++ src/test/java/com/capgeminitrainning/AddressBookApp/AddressBookAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgeminitrainning.AddressBookApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressBookAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
