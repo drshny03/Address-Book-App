011144d0750a44693c88de408611eb70cfbc3a7f
drshny03 <darshanyadav7049@gmail.com>
2025-03-01 11:13:38 +0530

    [Darshan] Add . Use Lombok Library to auto generate getters and setter for the DTO

--- src/main/java/com/capgeminitrainning/AddressBookApp/dto/AddressDTO.java
+++ src/main/java/com/capgeminitrainning/AddressBookApp/dto/AddressDTO.java
@@ -1,47 +1,21 @@
 package com.capgeminitrainning.AddressBookApp.dto;
 
 import org.springframework.stereotype.Component;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
 
 @Component
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class AddressDTO {
-    //attribute
     //Attributes
     private int id;
     private String name;
     private String email;
     private long number;
-
-    //constructor
-    public AddressDTO() {}
-    public AddressDTO(int id, String name, String email, long number) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.number = number;
-    }
-    //getter and setter
-    public int getId() {
-        return id;
-    }
-    public void setId(int id) {
-        this.id = id;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public long getNumber() {
-        return number;
-    }
-    public void setNumber(long number) {
-        this.number = number;
-    }
 }
--- src/main/java/com/capgeminitrainning/AddressBookApp/model/AddressEntity.java
+++ src/main/java/com/capgeminitrainning/AddressBookApp/model/AddressEntity.java
@@ -2,49 +2,15 @@ package com.capgeminitrainning.AddressBookApp.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import org.springframework.stereotype.Component;
+import lombok.Data;
 
 @Entity
-@Component
-public class AddressEntity {
+public @Data class AddressEntity
+{
     //Attributes
     @Id
     private int id;
     private String name;
     private String email;
     private long number;
-
-    //constructor
-    public AddressEntity() {}
-    public AddressEntity(int id, String name, String email, long number) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.number = number;
-    }
-    //getter and setter
-    public int getId() {
-        return id;
-    }
-    public void setId(int id) {
-        this.id = id;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public long getNumber() {
-        return number;
-    }
-    public void setNumber(long number) {
-        this.number = number;
-    }
 }
