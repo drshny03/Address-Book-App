0bd0743680d2002f72d65542188ae42433666bd7
drshny03 <darshanyadav7049@gmail.com>
2025-03-01 11:34:55 +0530

    Add . Use Lambok Library for Logging

--- src/main/java/com/capgeminitrainning/AddressBookApp/controller/AddressController.java
+++ src/main/java/com/capgeminitrainning/AddressBookApp/controller/AddressController.java
@@ -6,10 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
-
+@Slf4j
 @RestController
 @RequestMapping("/address")
 public class AddressController {
@@ -24,27 +28,32 @@ public class AddressController {
     //method to get all address
     @GetMapping("/")
     public ResponseEntity<Map<Integer, AddressDTO>> getAllAddress(){
+        log.info("receive rest request for retrieving all addresses");
         return addressService.getAllAddress();
     }
     //method to get address by id
     @GetMapping("/get/{id}")
     public ResponseEntity<AddressDTO> getAddressById(@PathVariable int id){
+        log.info("receive rest request for retrieve address with id {}",id);
         return addressService.getAddressById(id);
     }
     //method to post address
     @PostMapping("/post")
     public ResponseEntity<String> postAddress(@RequestBody AddressDTO address){
+        log.info("receive rest request to add new address");
         return addressService.postAddress(address);
     }
     //method to put address
     @PutMapping("/put")
     public ResponseEntity<String> putAddress(@RequestParam int id,
                                              @RequestBody AddressDTO address){
+        log.info("receive rest request for update address with id {}", id);
         return addressService.putAddress(id,address);
     }
     //method to delete address
     @DeleteMapping("/delete")
     public ResponseEntity<String> deleteAddress(@RequestParam int id){
+        log.info("receive rest request to delete address with id {}", id);
         return addressService.deleteAddress(id);
     }
 
--- src/main/java/com/capgeminitrainning/AddressBookApp/service/AddressService.java
+++ src/main/java/com/capgeminitrainning/AddressBookApp/service/AddressService.java
@@ -1,41 +1,34 @@
 package com.capgeminitrainning.AddressBookApp.service;
 
 import com.capgeminitrainning.AddressBookApp.dto.AddressDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @Service
 @Component
 public class AddressService {
-    private static final Logger logger = LoggerFactory.getLogger(AddressService.class);
     //map to store address
     Map<Integer, AddressDTO> addressMap;
 
-
     //constructor
-    @Autowired
     public AddressService(){
-
         addressMap = new HashMap<>();
     }
 
     //service for get all address
     public ResponseEntity<Map<Integer, AddressDTO>> getAllAddress() {
-        logger.info("retrieving all address.....");
+        log.info("retrieving all address.....");
         return ResponseEntity.ok(addressMap);
     }
     //service for get address by id
     public ResponseEntity<AddressDTO> getAddressById(int id){
-        logger.info("retrieving address by id.....");
+        log.info("retrieving address for id {}" , id);
         //check if id present
         if(addressMap.containsKey(id)){
             AddressDTO address = addressMap.get(id);
@@ -45,6 +38,7 @@ public class AddressService {
     }
     //service for update address
     public ResponseEntity<String> putAddress(int id, AddressDTO addressDTO){
+        log.info("updating address for id {}" , id);
         //check if id present
         if(!addressMap.containsKey(id)){
             return ResponseEntity.ok("id not found");
@@ -54,12 +48,14 @@ public class AddressService {
     }
     //service to post address
     public ResponseEntity<String> postAddress(AddressDTO address) {
+        log.info("Adding new address to address book");
         int id = address.getId();
         addressMap.put(id,address);
         return ResponseEntity.ok("address stored successfully");
     }
     //service for delete address
     public ResponseEntity<String> deleteAddress(int id) {
+        log.info("deleting address for id {}" , id);
         //check if id present
         if(!addressMap.containsKey(id)){
             return ResponseEntity.ok("id not found");
